ab94f47ff52655c38de59c93ea99ec9e93319537
carsten <c.krumpholz@cakru-it.com>
2023-08-20 13:52:46 +0300

    Install Folderstructure for docker install odoo 16

---
+++ docker/postgres/init/init.sql
@@ -0,0 +1,11 @@
+-- Create User odoo_15_0_live
+CREATE USER odoo_live WITH PASSWORD 'Gp=268vLy^LMJmA2NKqv7>nT!-5>ftom}YJzTeeiZ';
+ALTER USER odoo_15_0_live CREATEDB;
+
+-- Create User odoo_15_0_pre
+--CREATE USER odoo_15_0_pre WITH PASSWORD 'Gp=268vLy^LMJmA2NKqv7>nT!-5>ftom}YJzTeeiZ';
+--ALTER USER odoo_15_0_pre CREATEDB;
+
+-- Create User odoo_15_0_stage
+CREATE USER odoo_stage WITH PASSWORD 'Gp=268vLy^LMJmA2NKqv7>nT!-5>ftom}YJzTeeiZ';
+ALTER USER odoo_15_0_stage CREATEDB;
