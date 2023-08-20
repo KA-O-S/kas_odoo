ac7b0a07136e8ff242519e86a8cdd4c7dcc7d2f6
carsten <c.krumpholz@cakru-it.com>
2023-08-20 16:38:42 +0300

    set user on DB

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,6 +1,6 @@
 -- Create User odoo_15_0_live
 CREATE USER odoo_live WITH PASSWORD 'Gp=268vLy^LMJmA2NKqv7>nT!-5>ftom}YJzTeeiZ';
-ALTER USER odoo_15_0_live CREATEDB;
+ALTER USER odoo_live CREATEDB;
 
 -- Create User odoo_15_0_pre
 --CREATE USER odoo_15_0_pre WITH PASSWORD 'Gp=268vLy^LMJmA2NKqv7>nT!-5>ftom}YJzTeeiZ';
@@ -8,4 +8,4 @@ ALTER USER odoo_15_0_live CREATEDB;
 
 -- Create User odoo_15_0_stage
 CREATE USER odoo_stage WITH PASSWORD 'Gp=268vLy^LMJmA2NKqv7>nT!-5>ftom}YJzTeeiZ';
-ALTER USER odoo_15_0_stage CREATEDB;
+ALTER USER odoo_stage CREATEDB;
