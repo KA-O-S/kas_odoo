c0c23c69d9662d85e10fbf5b333c0bc30d00d1a1
carsten <c.krumpholz@cakru-it.com>
2024-03-06 16:35:05 +0200

    Anpassung für kbnb

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,8 +1,8 @@
 -- Create User odoo_live odoo.conf --> db_password
-CREATE USER odoo_live WITH PASSWORD 'Gp=268vLy^LMJmA2NKqv7>nT!-5>ftom}YJzTeeiZ';
+CREATE USER odoo_live WITH PASSWORD 'cT!Cft&6YW,))wLZ~GeyzWQad-qz_3+<+m';
 ALTER USER odoo_live CREATEDB;
 
 
 -- Create User odoo_stage odoo.conf --> db_password
-CREATE USER odoo_stage WITH PASSWORD 'Gp=268vLy^LMJmA2NKqv7>nT!-5>ftom}YJzTeeiZ';
+CREATE USER odoo_stage WITH PASSWORD 'cT!Cft&6YW,))wLZ~GeyzWQad-qz_3+<+m';
 ALTER USER odoo_stage CREATEDB;
